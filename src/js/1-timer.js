9f940516d3884daf992555a04c0ece4ddbbcf66a
alona007 <alona.olyksashenko@gmail.com>
2024-05-03 21:51:45 +0200

    4

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -49,11 +49,11 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
- 
   const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);  
-  const minutes = Math.floor(((ms % day) % hour) / minute);  
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
   return { days, hours, minutes, seconds };
 }
 
